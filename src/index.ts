c6f1e80212ef2dca6ca5ee564e4043832f4af5e1
khalil <khalilbenmeziane@gmail.com>
2025-09-29 14:58:35 +0100

    update/ addes some route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
-import { renderHtml } from "./renderHtml";
+import { Hono } from "hono";
 
-export default {
-  async fetch(request, env) {
-    const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
-    const { results } = await stmt.all();
+type Data = {
+  fullName: string;
+  phoneNumber?: string;
+  website?: string;
+  fbPage?: string;
+  igPage?: string;
+  tiktok?: string;
+};
 
-    return new Response(renderHtml(JSON.stringify(results, null, 2)), {
-      headers: {
-        "content-type": "text/html",
-      },
-    });
-  },
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{ Bindings: Env }>();
+
+app.post("/api/add", async (c) => {
+  const { fullName, fbPage, igPage, phoneNumber, tiktok, website } =
+    await c.req.json<Data>();
+  const stmnt = await c.env.DB.prepare(
+    "INSERT INTO users (fullName,phoneNumber,fbPage,igPage,tiktok,website) VALUES (?,?,?,?,?,?)"
+  )
+    .bind(fullName, phoneNumber, fbPage, igPage, tiktok, website)
+    .run();
+
+  return c.json({});
+});
+
+app.get("/api/customer/:id", async (c) => {
+  const userId = c.req.param("id");
+  const query = await c.env.DB.prepare("SELECT * FROM users WHERE id = ?")
+    .bind(userId)
+    .first();
+
+  return c.json(query);
+});
+
+export default app;
