fdb7bce94c99787516c3d39fe8a57bca87189222
khalil <khalilbenmeziane@gmail.com>
2025-09-30 17:10:50 +0100

    update/ final

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 type Data = {
   fullName: string;
@@ -11,25 +12,34 @@ type Data = {
 
 const app = new Hono<{ Bindings: Env }>();
 
+app.use("*", cors());
+
 app.post("/api/add", async (c) => {
   const { fullName, fbPage, igPage, phoneNumber, tiktok, website } =
     await c.req.json<Data>();
+  console.log(fullName, fbPage);
+
   const stmnt = await c.env.DB.prepare(
     "INSERT INTO users (fullName,phoneNumber,fbPage,igPage,tiktok,website) VALUES (?,?,?,?,?,?)"
   )
     .bind(fullName, phoneNumber, fbPage, igPage, tiktok, website)
     .run();
 
-  return c.json({});
+  return c.json({ userId: stmnt.meta.last_row_id }, 201);
 });
 
-app.get("/api/customer/:id", async (c) => {
-  const userId = c.req.param("id");
-  const query = await c.env.DB.prepare("SELECT * FROM users WHERE id = ?")
-    .bind(userId)
-    .first();
+app.get("/api/customers/:id", async (c) => {
+  try {
+    const userId = c.req.param("id");
+    const query = await c.env.DB.prepare("SELECT * FROM users WHERE id = ?")
+      .bind(userId)
+      .first();
+    if (!query) return c.text("not found", 404);
 
-  return c.json(query);
+    return c.json(query);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 export default app;
